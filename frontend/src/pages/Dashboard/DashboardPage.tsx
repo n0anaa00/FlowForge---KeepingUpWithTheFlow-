f6b22050b0c5ea9c2e84dae699f09fc345fe0421
Aarne Andersson <aarne.andersson@gmail.com>
2026-07-22 17:56:35 +0300

    dashboardpage

--- frontend/src/pages/Dashboard/DashboardPage.tsx
+++ frontend/src/pages/Dashboard/DashboardPage.tsx
@@ -1,25 +1,53 @@
-import { Typography } from "@mui/material";
+import {
+
+    Typography,
+
+    Box
+
+} from "@mui/material";
 
 import AppLayout from "../../components/layout/AppLayout";
+
+import StatsGrid from "../../components/dashboard/StatsGrid";
+
 import { useAuth } from "../../context/AuthContext";
 
 function DashboardPage() {
-  const { user } = useAuth();
 
-  return (
-    <AppLayout>
-      <Typography variant="h4">
-        Welcome back, {user?.name.split(" ")[0]}
-      </Typography>
+    const { user } = useAuth();
 
-      <Typography
-        color="text.secondary"
-        sx={{ mt: 1 }}
-      >
-        Ready to enter the flow?
-      </Typography>
-    </AppLayout>
-  );
+    return (
+
+        <AppLayout>
+
+            <Typography variant="h4">
+
+                Welcome back, {user?.name.split(" ")[0]}
+
+            </Typography>
+
+            <Typography
+
+                color="text.secondary"
+
+                mb={4}
+
+            >
+
+                Ready to enter the flow?
+
+            </Typography>
+
+            <Box>
+
+                <StatsGrid />
+
+            </Box>
+
+        </AppLayout>
+
+    );
+
 }
 
 export default DashboardPage;
